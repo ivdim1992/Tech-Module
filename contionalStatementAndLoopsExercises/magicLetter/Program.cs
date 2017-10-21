088e6afc13c762bdc6f6d01bc4e2d47b00fde696
Ivaylo <ivdim1992@gmail.com>
2017-10-21 11:27:31 +0300

    changes magicLetters and add neightbourWars

--- contionalStatementAndLoopsExercises/magicLetter/Program.cs
+++ contionalStatementAndLoopsExercises/magicLetter/Program.cs
@@ -10,6 +10,25 @@ namespace magicLetter
     {
         static void Main(string[] args)
         {
+            char firstLetter = char.Parse(Console.ReadLine());
+            char secondLetter = char.Parse(Console.ReadLine());
+            char thirdLetter = char.Parse(Console.ReadLine());
+
+            for (char i = firstLetter; i <= secondLetter; i++)
+            {
+                for (char j = firstLetter; j <= secondLetter; j++)
+                {
+                    for (char k = firstLetter; k <= secondLetter; k++)
+                    {
+                        string word = i.ToString() + j.ToString() + k.ToString();
+
+                        if (!word.Contains(thirdLetter))
+                        {
+                            Console.Write(word + " ");
+                        }
+                    }
+                }
+            }
         }
     }
 }
---
+++ contionalStatementAndLoopsExercises/neighbourWars/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace neighbourWars
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int peshoDamage = int.Parse(Console.ReadLine());
+            int goshoDamage = int.Parse(Console.ReadLine());
+            int peshoHealth = 100;
+            int goshoHealth = 100;
+            int round = 0;
+            for (int i = 1; i < 100; i++)
+            {
+                if (i % 2 != 0)
+                {
+                    round++;
+                    goshoHealth -= peshoDamage;
+                    if (goshoHealth <= 0)
+                    {
+                        break;
+                    }
+                    Console.WriteLine($"Pesho used Roundhouse kick and reduced Gosho to {goshoHealth} health.");                  
+                }
+                else
+                {
+                    round++;
+                    peshoHealth -= goshoDamage;
+                    if (peshoHealth <= 0)
+                    {
+                        break;
+                    }
+                    Console.WriteLine($"Gosho used Thunderous fist and reduced Pesho to {peshoHealth} health.");
+                }            
+
+                if (i % 3 == 0)
+                {
+                    peshoHealth += 10;
+                    goshoHealth += 10;
+                }
+            }
+
+            if (peshoHealth <= 0)
+            {
+                Console.WriteLine($"Gosho won in {round}th round.");
+                
+            }
+            else if (goshoHealth <= 0)
+            {
+                Console.WriteLine($"Pesho won in {round}th round.");
+                
+            }
+
+        }
+    }
+}
